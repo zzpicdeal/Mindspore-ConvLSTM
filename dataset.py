bbe6c7eb56ba84a81b6732274ae1246a8da69d9b
lmh447669785 <447669785@qq.com>
2022-07-20 20:24:11 +0800

    上传文件至 ''

--- dataset.py
+++ dataset.py
@@ -156,7 +156,20 @@ class MovingMNIST():
 
     def __len__(self):
         return self.length
+def _get_rank_info():
+    """
+    get rank size and rank id
+    """
+    rank_size = int(os.environ.get("RANK_SIZE", 1))
 
+    if rank_size > 1:
+        from mindspore.communication.management import get_rank, get_group_size
+        rank_size = get_group_size()
+        rank_id = get_rank()
+    else:
+        rank_size = rank_id = None
+
+    return rank_size, rank_id
 random_seed = 1996
 np.random.seed(random_seed)
 
@@ -165,13 +178,23 @@ def create_train_dataset(dataset,batch=32):
       Create train dataset.
     '''
 
+    device_num, rank_id = _get_rank_info()
 
+    if device_num == 1 :
 
-    train_ds = ds.GeneratorDataset(dataset, column_names=["input_images", "target_images"], shuffle=False)
+        train_ds = ds.GeneratorDataset(dataset, column_names=["input_images", "target_images"], shuffle=False,num_parallel_workers=8)
+    else:
+        train_ds = ds.GeneratorDataset(dataset, column_names=["input_images", "target_images"], shuffle=False,num_parallel_workers=8,
+                                         num_shards=device_num, shard_id=rank_id)
+
+    
     train_ds = train_ds.batch(batch, drop_remainder=True)
 
     return train_ds
+
 class get_dataset():
     def __init__(self,root,batch=32) -> None:
         self.train_dataset = create_train_dataset(MovingMNIST(root,True,10,10,[3],True),batch)
         self.val_dataset = create_train_dataset(MovingMNIST(root,False,10,10,[3],True),batch)
+
+
