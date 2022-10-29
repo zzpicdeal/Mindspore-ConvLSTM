76227b50d4753fbaf2c35af2407c5b5a05686d97
lmh447669785 <447669785@qq.com>
2022-10-29 22:12:41 +0800

    更新 'train_.py'

--- train_.py
+++ train_.py
@@ -10,6 +10,7 @@ import  glob
 import  numpy as np
 import  logging
 import  argparse
+
 from mindspore import context
 from mindspore.train.callback import ModelCheckpoint, CheckpointConfig, LossMonitor
 from mindspore import Tensor, Model
@@ -42,11 +43,11 @@ parser.add_argument('--batch_size', type=int, default=32, help='batch size')
 
 parser.add_argument('--epochs', type=int, default=100, help='num of training epochs')
 
-parser.add_argument('--data_url',
+parser.add_argument('--data_path',
                     help='path to training/inference dataset folder',
                     default= workroot + '/data/')
 
-parser.add_argument('--train_url',
+parser.add_argument('--train_path',
                     help='model folder to save/load',
                     default= workroot + '/model/')#save_path
 
@@ -60,14 +61,7 @@ parser.add_argument(
     choices=['Ascend', 'CPU'],
     help='device where the code will be implemented (default: CPU),若要在启智平台上使用NPU，需要在启智平台训练界面上加上运行参数device_target=Ascend')
 args = parser.parse_args()
-#context.set_context(mode=context.PYNATIVE_MODE, device_target="Ascend")
-#context.set_context(mode=context.GRAPH_MODE, device_target="Ascend")
-
-#device_id = int(os.getenv('DEVICE_ID'))
 context.set_context(mode=context.GRAPH_MODE, device_target="Ascend")
-#context.set_context(device_id=device_id) # set device_id
-#init()
-
 random_seed = 1996
 np.random.seed(random_seed)
 
@@ -95,12 +89,12 @@ def main():
     rank =1
     print(os.listdir(workroot))
     #初始化数据和模型存放目录
-    data_dir = workroot + '/data'  #先在训练镜像中定义数据集路径
-    train_dir = workroot + '/model' #先在训练镜像中定义输出路径
+    data_dir = args.data_path  #先在训练镜像中定义数据集路径
+    train_dir = args.train_path #先在训练镜像中定义输出路径
     if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
+        raise
     if not os.path.exists(train_dir):
-            os.makedirs(train_dir)
+        os.makedirs(train_dir)
  ######################## 将数据集从obs拷贝到训练镜像中 （固定写法）########################   
     # 在训练环境中定义data_url和train_url，并把数据从obs拷贝到相应的固定路径，以下写法是将数据拷贝到/home/work/user-job-dir/data/目录下，可修改为其他目录
     #ObsToEnv(args.data_url,data_dir)
